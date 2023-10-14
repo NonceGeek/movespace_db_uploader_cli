43a031d1cd00be4e8c7367817bba5770f617b5da
LiAohua <liaohua@LiAohuadeMacBook-Pro.local>
2023-10-14 15:10:24 +0800

    init

---
+++ lib/movespace_db_uploader_cli.ex
@@ -0,0 +1,18 @@
+defmodule MovespaceDbUploaderCli.CLI do
+  @moduledoc """
+    TODO: upload data to movespaceDB by CLI.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MovespaceDbUploaderCli.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/movespace_db_uploader_cli_test.exs
@@ -0,0 +1,8 @@
+defmodule MovespaceDbUploaderCliTest do
+  use ExUnit.Case
+  doctest MovespaceDbUploaderCli
+
+  test "greets the world" do
+    assert MovespaceDbUploaderCli.hello() == :world
+  end
+end
